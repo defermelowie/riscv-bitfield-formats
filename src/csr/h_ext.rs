a4e09f0217f59b664d8a60ffde7934f29453af97
Lowie Deferme <lowie.deferme@student.kuleuven.be>
2023-04-24 12:19:02 +0200

    feat: Added hstatus

--- src/csr/h_ext.rs
+++ src/csr/h_ext.rs
@@ -1,59 +1,59 @@
-// //! CSR definitions for the hypervisor extension
-// use super::base::dec_arch;
-// use super::{get_bit, get_bits, Csr};
+//! CSR definitions for the hypervisor extension
+use crate::bit::BitField;
 
-// /**************************************************************/
-// /* Hypervisor Status Register                                 */
+use super::base::dec_arch;
+use super::Csr;
 
-// pub struct Hstatus {
-//     pub vsbe: B1,
-//     pub gva: B1,
-//     pub spv: B1,
-//     pub spvp: B1,
-//     pub hu: B1,
-//     pub vgein: B6,
-//     pub vtvm: B1,
-//     pub vtw: B1,
-//     pub vtsr: B1,
-//     pub vsxl: B2,
-// }
+/**************************************************************/
+/* Hypervisor Status Register                                 */
+pub struct Hstatus {
+    pub vsbe: BitField<5, 5>,
+    pub gva: BitField<6, 6>,
+    pub spv: BitField<7, 7>,
+    pub spvp: BitField<8, 8>,
+    pub hu: BitField<9, 9>,
+    pub vgein: BitField<12, 17>,
+    pub vtvm: BitField<20, 20>,
+    pub vtw: BitField<21, 21>,
+    pub vtsr: BitField<22, 22>,
+    pub vsxl: BitField<32, 33>,
+}
 
-// impl Csr for Hstatus {
-//     fn new(value: u64) -> Self {
-//         Hstatus {
-//             vsbe: B1(get_bit(value, 5)),
-//             gva: B1(get_bit(value, 6)),
-//             spv: B1(get_bit(value, 7)),
-//             spvp: B1(get_bit(value, 8)),
-//             hu: B1(get_bit(value, 9)),
-//             vgein: B6(get_bits(value, 12, 17)),
-//             vtvm: B1(get_bit(value, 20)),
-//             vtw: B1(get_bit(value, 21)),
-//             vtsr: B1(get_bit(value, 22)),
-//             vsxl: B2(get_bits(value, 32, 33)),
-//         }
-//     }
+impl Csr for Hstatus {
+    fn new(value: u64) -> Self {
+        Hstatus {
+            vsbe: value.into(),
+            gva: value.into(),
+            spv: value.into(),
+            spvp: value.into(),
+            hu: value.into(),
+            vgein: value.into(),
+            vtvm: value.into(),
+            vtw: value.into(),
+            vtsr: value.into(),
+            vsxl: value.into(),
+        }
+    }
 
-//     fn print(&self) {
-//         println!("");
-//         println!("hstatus");
-//         println!("-------");
-//         println!("VSBE: {}", &self.vsbe);
-//         println!("GVA: {}", &self.gva);
-//         println!("SPV: {}", &self.spv);
-//         println!("SPVP: {}", &self.spvp);
-//         println!("HU: {}", &self.hu);
-//         println!("VGEIN: {}", &self.vgein);
-//         println!("VTVM: {}", &self.vtvm);
-//         println!("VTW: {}", &self.vtw);
-//         println!("VTSR: {}", &self.vtsr);
-//         println!("VSXL: {}", dec_arch(self.vsxl.to_u64()));
-//     }
-// }
+    fn print(&self) {
+        println!("");
+        println!("hstatus");
+        println!("-------");
+        println!("VSBE: {}", &self.vsbe);
+        println!("GVA: {}", &self.gva);
+        println!("SPV: {}", &self.spv);
+        println!("SPVP: {}", &self.spvp);
+        println!("HU: {}", &self.hu);
+        println!("VGEIN: {}", &self.vgein);
+        println!("VTVM: {}", &self.vtvm);
+        println!("VTW: {}", &self.vtw);
+        println!("VTSR: {}", &self.vtsr);
+        println!("VSXL: {}", dec_arch(self.vsxl.value()));
+    }
+}
 
 // /**************************************************************/
 // /* Hypervisor Trap Delegation Registers                       */
-
 // pub struct Hedeleg(B64);
 
 // impl Csr for Hedeleg {
