a4239ec36036a3ffb384d1938237524595b48d02
Lowie Deferme <16416567+defermelowie@users.noreply.github.com>
2024-02-09 15:19:10 +0100

    ci: add python dependency (#4)

--- build.rs
+++ build.rs
@@ -3,21 +3,28 @@ use std::process::Command;
 const OPCODES: &str = "res/opcodes";
 
 /// Build script:
-///   - Create inst.rs & copy to src/encoding.rs
+///   - Create src/encoding.rs
 ///
 fn main() {
     // Only rerun if opcode submodule changes
     println!("cargo:rerun-if-changed={OPCODES}/*");
+    // Install python dependencies
+    Command::new("pip3")
+        .current_dir(OPCODES)
+        .arg("install")
+        .arg("-r")
+        .arg("requirements.txt")
+        .status().expect("Failed to install python dependencies");
     // Create inst.rs
     Command::new("python3")
         .current_dir(OPCODES)
         .arg("parse.py")
         .arg("-rust")
         .arg("rv*")
-        .status().unwrap();
+        .status().expect("Failed to create");
     // Copy result to src/encoding.rs
     Command::new("cp")
         .arg(format!("{OPCODES}/inst.rs"))
         .arg("src/encoding.rs")
-        .status().unwrap();
+        .status().expect("Failed to copy");
 }
